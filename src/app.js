2c735b44ee4f1090ed90f0b030de9564c79ac238
BrianR <briomero76@gmail.com>
2022-02-13 12:42:08 -0500

    Main file app has been finished

---
+++ src/app.js
@@ -0,0 +1,47 @@
+import Home         from './views/pages/Home.js'
+import About        from './views/pages/About.js'
+import Error404     from './views/pages/Error404.js'
+import PostShow     from './views/pages/PostShow.js'
+import Register     from './views/pages/Register.js'
+
+import Navbar       from './views/components/Navbar.js'
+import Bottombar    from './views/components/Bottombar.js' 
+
+import Utils        from './services/Utils.js'
+ 
+//List of supported routes
+const routes = {
+    '/'             : Home
+    , '/about'      : About
+    , '/p/:id'      : PostShow
+    , '/register'   : Register
+}
+
+//Takes the URL and check with the list of supported routes
+const router = async () => {
+
+    const header = null || document.getElementById('header_container');
+    const content = null || document.getElementById('page_container');
+    const footer = null || document.getElementById('footer_container');
+    
+    // Render the Header and footer
+    header.innerHTML = await Navbar.render();
+    await Navbar.after_render();
+    footer.innerHTML = await Bottombar.render();
+    await Bottombar.after_render();
+
+    let request = Utils.parseRequestURL()
+
+    // Parse the URL and if it has an id part, change it with the string ":id"
+    let parsedURL = (request.resource ? '/' + request.resource : '/') + (request.id ? '/:id' : '') + (request.verb ? '/' + request.verb : '')
+    
+    let page = routes[parsedURL] ? routes[parsedURL] : Error404
+    content.innerHTML = await page.render();
+    await page.after_render();
+}
+
+// Listen on hash change:
+window.addEventListener('hashchange', router);
+
+// Listen on page load:
+window.addEventListener('load', router);
